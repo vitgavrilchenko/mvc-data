e2b92418ccf5fe3db6dbd0e6b4d28346dd166830
vitgavrilchenko <vitgavrilchenko@gmail.com>
2024-08-16 12:14:19 +0300

    mvc+bd

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+        List<Employee> theEmployees = employeeService.findAll();
+
+        theModel.addAttribute("employees", theEmployees);
+
+        return "list-employees";
+    }
+}
